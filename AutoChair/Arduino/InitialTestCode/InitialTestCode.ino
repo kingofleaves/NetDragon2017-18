5fddc8ac7f64cce9523b2d74271a20d196e25d51
wonsong94 <wonsong94@gmail.com>
2017-11-27 16:36:36 -0800

    added setup output pins

--- AutoChair/Arduino/InitialTestCode/InitialTestCode.ino
+++ AutoChair/Arduino/InitialTestCode/InitialTestCode.ino
@@ -2,20 +2,22 @@
 #define PIN_IN1_L 12
 #define PIN_IN2_L 13
 
-#define PIN_EN_L 10
-#define PIN_IN1_L 9
-#define PIN_IN2_L 8
+#define PIN_EN_R 10
+#define PIN_IN1_R 9
+#define PIN_IN2_R 8
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PIN_POWER, OUTPUT);
-  pinMode(PIN_GND, OUTPUT);  
+  pinMode(PIN_EN_L, OUTPUT);
+  pinMode(PIN_IN1_L, OUTPUT);  
+  pinMode(PIN_IN2_L, OUTPUT);  
+  pinMode(PIN_EN_R, OUTPUT);
+  pinMode(PIN_IN1_R, OUTPUT);  
+  pinMode(PIN_IN2_R, OUTPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(PIN_POWER, HIGH);
-  digitalWrite(PIN_GND, LOW);  
 }
