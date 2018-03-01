d84c99374a5b82028b8aeb6d2a92c0e9a07852f6
Glenn M. Davis <gmdavis1@gmail.com>
2018-02-28 20:12:28 -0800

    consolidated for python3

--- Winter/SpeechRecog/sloth.py
+++ Winter/SpeechRecog/sloth.py
@@ -1,77 +1,80 @@
 #!/usr/bin/env python3
 
+import http.client
+import urllib.parse
+import json
+import speech_recognition as sr
+from xml.etree import ElementTree
+
+
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
-import speech_recognition as sr
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "11dfb0a39e174cdbbb5d4b35cf6a16a5"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+
+
+#Note: The way to get api key:
+#Free: https://www.microsoft.com/cognitive-services/en-us/subscriptions?productId=/products/Bing.Speech.Preview
+#Paid: https://portal.azure.com/#create/Microsoft.CognitiveServices/apitype/Bing.Speech/pricingtier/S0
+
+params = ""
+headers = {"Ocp-Apim-Subscription-Key": BING_KEY}
+
+#AccessTokenUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+AccessTokenHost = "api.cognitive.microsoft.com"
+path = "/sts/v1.0/issueToken"
+
+# Connect to server to get the Access Token
+print ("Connect to server to get the Access Token")
+conn = http.client.HTTPSConnection(AccessTokenHost)
+conn.request("POST", path, params, headers)
+response = conn.getresponse()
+print(response.status, response.reason)
+
+data = response.read()
+conn.close()
+
+accesstoken = data.decode("UTF-8")
+print ("Access Token: " + accesstoken)
+
+body = ElementTree.Element('speak', version='1.0')
+body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+voice = ElementTree.SubElement(body, 'voice')
+voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
+voice.set('{http://www.w3.org/XML/1998/namespace}gender', 'Female')
+voice.set('name', 'Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)')
+voice.text = 'This is a demo to call microsoft text to speech service in Python.'
+
+headers = {"Content-type": "application/ssml+xml",
+            "X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+            "Authorization": "Bearer " + accesstoken,
+            "X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+            "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
+            "User-Agent": "TTSForPython"}
+
+#Connect to server to synthesize the wave
+print("\nConnect to server to synthesize the wave")
+conn = http.client.HTTPSConnection("speech.platform.bing.com")
+conn.request("POST", "/synthesize", ElementTree.tostring(body), headers)
+response = conn.getresponse()
+print(response.status, response.reason)
+
+data = response.read()
+conn.close()
+print("The synthesized wave length: %d" %(len(data)))
 
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-# # recognize speech using Sphinx
-# try:
-#     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-# except sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
-
-# # recognize speech using Google Speech Recognition
-# try:
-#     # for testing purposes, we're just using the default API key
-#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-#     # instead of `r.recognize_google(audio)`
-#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-# except sr.UnknownValueError:
-#     print("Google Speech Recognition could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# # recognize speech using Google Cloud Speech
-# GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-# try:
-#     print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-# except sr.UnknownValueError:
-#     print("Google Cloud Speech could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
-# # recognize speech using Wit.ai
-# WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-# try:
-#     print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-# except sr.UnknownValueError:
-#     print("Wit.ai could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Wit.ai service; {0}".format(e))
-
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "11dfb0a39e174cdbbb5d4b35cf6a16a5"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# # recognize speech using Houndify
-# HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
-# HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
-# try:
-#     print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-# except sr.UnknownValueError:
-#     print("Houndify could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Houndify service; {0}".format(e))
-
-# # recognize speech using IBM Speech to Text
-# IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-# IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-# try:
-#     print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-# except sr.UnknownValueError:
-#     print("IBM Speech to Text could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from IBM Speech to Text service; {0}".format(e))
+    while True:
+        print("Say something!")
+        audio = r.listen(source)
+        user_text = r.recognize_bing(audio, key=BING_KEY)
+        try:
+            print("Microsoft Bing Voice Recognition thinks you said " + user_text)
+        except sr.UnknownValueError:
+            print("Microsoft Bing Voice Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+        if user_text == "End program.":
+            raise SystemExit
