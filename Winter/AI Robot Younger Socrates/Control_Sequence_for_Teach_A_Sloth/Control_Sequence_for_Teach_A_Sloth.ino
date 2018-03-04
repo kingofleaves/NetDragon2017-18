fb03fd0bf805f141bfc0151f0120516d3ae60a88
kingofleaves <wangye@stanford.edu>
2018-03-03 17:09:15 -0800

    [WIP] left arm moves :D Time to integrate head and arm.

--- Winter/AI Robot Younger Socrates/Control_Sequence_for_Teach_A_Sloth/Control_Sequence_for_Teach_A_Sloth.ino
+++ Winter/AI Robot Younger Socrates/Control_Sequence_for_Teach_A_Sloth/Control_Sequence_for_Teach_A_Sloth.ino
@@ -6,6 +6,13 @@
 
 #include <Servo.h>
 #include <Stepper.h>
+#include <SoftwareSerial.h>
+
+// Bluetooth SoftwareSerial
+static const int TxD = 2;
+static const int RxD = 3;
+SoftwareSerial btSerial(TxD, RxD);
+
 
 // Nodding Control - Stepper Motor 
 static const int stepsPerRevolution = 48;  // change this to fit the number of steps per Loop
@@ -20,71 +27,96 @@ bool isNodding = false; // switch for whether sloth is nodding (servo)
 bool nodDownwards = false;
 
 // Jaw Control - single servo
-static const int jawPin = 3;
+static const int jawPin = 10;
 Servo jawServo;  //servo for moving jaw for speaking
 int jawPos = 0;    // variable to store the servo position for speaking
 int jawPosInit = 0;
 bool isSpeaking = false; // switch for whether sloth is speaking (servo)
 
-// Arms Control - two servos
-bool isFlailing = false;
-
-static const int lArmPin = 8;
-Servo lArmServo;
-int lArmPos = 0;    // variable to store the servo position for speaking
-int lArmPosInit = 0;
-bool lArmMoving = false; // switch for whether sloth is speaking (servo)
-
+// Arm Control - servo on left arm
 static const int rArmPin = 9;
 Servo rArmServo;
 int rArmPos = 0;    // variable to store the servo position for speaking
 int rArmPosInit = 0;
 bool rArmMoving = false; // switch for whether sloth is speaking (servo)
+#define ARM_IN 0
+#define ARM_USER 90
+#define ARM_SCREEN 180
 
 
 void setup() {
   Serial.begin(9600);
+  btSerial.begin(9600);
   nodStepper.setSpeed(nodSpeed);
   jawServo.attach(jawPin);  
   rArmServo.attach(rArmPin);  
-  lArmServo.attach(lArmPin);  
 }
 
 void loop() {
-  if (isNodding) {
-    nod();
-  }
-  if (isSpeaking) {
-    moveJaw();
-  }
-  if (isFlailing) {
-    flail();
-  }
-  
+//  if (listening) {
+//    nod();
+//  }
+//  if (thinking) {
+//    think();
+//  }
+//  if (speaking) {
+//    moveJaw();
+//  }
+//  if (screen) {
+//    pointToScreen();
+//  }
+//  
   checkInput();
 }
 
 void checkInput(void) {
+  char c = 0;
   if (Serial.available()) {
-    char c = Serial.read();
-    Serial.println(c);
-    switch (c) {
-      case 'n':
-        Serial.println("nod");
-        isNodding = !isNodding;
-        break;
-      case 's':
-        isSpeaking = !isSpeaking;
-        jawPos = jawPosInit;
-        moveJaw();
-        break;
-      case 'f':
-        isFlailing = !isFlailing;
-        break; 
-      case 't':
-        // tilt head;
-        break; 
-    }
+    c = Serial.read();
+  }
+  if (btSerial.available()) {
+    c = btSerial.read();
+  }
+  if (c == 0) return;
+  Serial.println(c);
+  switch (c) {
+    case 'n':
+      Serial.println("nod");
+      isNodding = !isNodding;
+      break;
+    case 's':
+      isSpeaking = !isSpeaking;
+      jawPos = jawPosInit;
+      moveJaw();
+      break;
+    case 't':
+      // tilt head;
+      break; 
+    case 'a':
+      // debug
+      moveArm(0);
+      break;
+    case 'b':
+      // debug
+      moveArm(90);
+      break;
+    case 'c':
+      // debug
+      moveArm(180);
+      break;
+    case 'd':
+      // debug
+      moveArm(270);
+      break;
+    case 'e':
+      // debug
+      moveArm(360);
+      break;
+    case 'z':
+      // debug
+      moveArm(450);
+      break;
+    
   }
 }
 
@@ -95,6 +127,13 @@ void nod(void) {
     nodDownwards = !nodDownwards; 
 }
 
+void think(void) {
+    // Move head down 
+    
+    // Move arm in
+    moveArm(ARM_IN);
+}
+
 void moveJaw(void) {
   if (jawPos > 360) jawPos -= 360;
   int servoInput = (jawPos > 180 ? 360 - jawPos : jawPos);
@@ -102,21 +141,14 @@ void moveJaw(void) {
   jawPos++;
 }
 
-void flail(void) {
-  Serial.println("Flailing");
-  moveRightArm();
-  moveLeftArm();
+void moveArm(int pos) {
+  rArmServo.write(pos);
 }
 
-void moveRightArm(void) {
+void swingRightArm(void) {
   if (rArmPos > 360) rArmPos -= 360;
   int servoInput = (rArmPos > 180 ? 360 - rArmPos : rArmPos);
   rArmServo.write(servoInput);              // tell servo to go to position in variable 'pos' 
   rArmPos++;
 }
-void moveLeftArm(void) {
-  if (lArmPos > 360) lArmPos -= 360;
-  int servoInput = (lArmPos > 180 ? 360 - lArmPos : lArmPos);
-  lArmServo.write(servoInput);              // tell servo to go to position in variable 'pos' 
-  lArmPos++;
-}
+
