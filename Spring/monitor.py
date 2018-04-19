9ba2379f054680c4c95c1fd8d726ec9051bd13ba
kingofleaves <wangye@stanford.edu>
2018-04-19 00:23:56 -0700

    copying to my computer

--- Spring/monitor.py
+++ Spring/monitor.py
@@ -1,4 +1,5 @@
 import serial
+import struct
 
 ser1 = serial.Serial("/dev/ttyACM+PATH1", 9600, timeout = 0)
 ser2 = serial.Serial("/dev/ttyACM+PATH2", 9600, timeout = 0)
@@ -14,10 +15,10 @@ ser4.open()
 # arduinos should fill up a buffer with floats, RPi will get one float at a
 # time from each port and process the graph
 
-total1 = 0.0
-total2 = 0.0
-total3 = 0.0
-total4 = 0.0
+total1 = double(0.0)
+total2 = double(0.0)
+total3 = double(0.0)
+total4 = double(0.0)
 try:
 	while True:
 		response1 = ser1.read(8)
@@ -25,13 +26,13 @@ try:
     	response3 = ser3.read(8)
     	response4 = ser4.read(8)
     	# gets 8 bytes (1 double) from each of the 4 serial ports
-    	# non-blocking mode, so get a float if one has been sent, otherwise
+    	# non-blocking mode, so get a double if one has been sent, otherwise
     	# move on to the next port
 
-		total1 += float(response1)
-		total2 += float(response2)
-		total3 += float(response3)
-		total4 += float(response4)
+		total1 += struct.unpack('d', response1)
+		total2 += struct.unpack('d', response2)
+		total3 += struct.unpack('d', response3)
+		total4 += struct.unpack('d', response4)
 
 		# display the contributions
 		
