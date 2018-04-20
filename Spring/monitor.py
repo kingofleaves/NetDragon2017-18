5bcd9f6e16683f0910477e8dd8901c0e44e68b24
Glenn M. Davis <gmdavis1@gmail.com>
2018-04-19 20:27:32 -0700

    reading floats from Arduino to RPi

--- Spring/monitor.py
+++ Spring/monitor.py
@@ -6,20 +6,10 @@ from collections import namedtuple
 import numpy as np
 import sys
 
-ser1 = serial.Serial("/dev/ttyACM+PATH1", 9600, timeout = 0)
-# ser2 = serial.Serial("/dev/ttyACM+PATH2", 9600, timeout = 0)
-# ser3 = serial.Serial("/dev/ttyACM+PATH3", 9600, timeout = 0)
-# ser4 = serial.Serial("/dev/ttyACM+PATH4", 9600, timeout = 0)
-# timeout = 0 means non-blocking mode, return immediately up to req
-# number of bytes
-
-ser1.open()
-# ser2.open()
-# ser3.open()
-# ser4.open()
-
-# arduinos should fill up a buffer with floats, RPi will get one float at a
-# time from each port and process the graph
+ser1 = serial.Serial("/dev/ttyACM0")
+ser2 = serial.Serial("/dev/ttyACM1")
+ser3 = serial.Serial("/dev/ttyACM2")
+ser4 = serial.Serial("/dev/ttyACM3")
 
 total1 = 0.0
 total2 = 0.0
@@ -36,24 +26,22 @@ error_config = {'ecolor': '0.3'}
 
 try:
     while True:
-        response1 = ser1.readline()
-        # response2 = ser2.readline()
-        # response3 = ser3.readline()
-        # response4 = ser4.readline()
-        # gets 8 bytes (1 double) from each of the 4 serial ports
-        # non-blocking mode, so get a double if one has been sent, otherwise
-        # move on to the next port
+        try:
+            response1 = float(ser1.readline())
+            response2 = float(ser2.readline())
+            response3 = float(ser3.readline())
+            response4 = float(ser4.readline())
 
-        total1 += struct.unpack('d', response1)
-        # total2 += struct.unpack('d', response2)
-        # total3 += struct.unpack('d', response3)
-        # total4 += struct.unpack('d', response4)
+        total1 += response1
+        total2 += response2
+        total3 += response3
+        total4 += response4
         # add the double value to each total
-        print total1
 
         totals = (total1, total2, total3, total4)
 
-        # display the contributions
+        # display the contributions with an updating bar graph
+
         # rects1 = ax.bar(index, totals, bar_width, alpha = opacity, color = 'b', error_kw = error_config)
 
         # ax.set_xlabel('Group member')
@@ -65,9 +53,10 @@ try:
 
         # fig.tight_layout()
         # plt.show()
-    # ser1.close()
-    # ser2.close()
-    # ser3.close()
-    # ser4.close()
+
+        # ser1.close()
+        # ser2.close()
+        # ser3.close()
+        # ser4.close()
 except:
     sys.exit(0)
