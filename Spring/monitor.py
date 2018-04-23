591482894efa716a5470def45e00bc313515f124
Glenn M. Davis <gmdavis1@gmail.com>
2018-04-22 18:07:26 -0700

    working real-time bar graph monitor

--- Spring/monitor.py
+++ Spring/monitor.py
@@ -1,67 +1,55 @@
 import serial
-import struct
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
-from collections import namedtuple
+import matplotlib.animation as animation
 import numpy as np
-import sys
+import random
 
-ser1 = serial.Serial("/dev/ttyACM0")
-ser2 = serial.Serial("/dev/ttyACM1")
-ser3 = serial.Serial("/dev/ttyACM2")
-ser4 = serial.Serial("/dev/ttyACM3")
+# ser1 = serial.Serial("/dev/ttyACM0")
+# ser2 = serial.Serial("/dev/ttyACM1")
+# ser3 = serial.Serial("/dev/ttyACM2")
+# ser4 = serial.Serial("/dev/ttyACM3")
 
 total1 = 0.0
 total2 = 0.0
 total3 = 0.0
 total4 = 0.0
+totals = [total1, total2, total3, total4]
 
-response1 = 0.0
-response2 = 0.0
-response3 = 0.0
-response4 = 0.0
+def refresh_totals(totals):
+    # add the float from Arduino for each totals[], now just simulated data
+    totals[0] += random.random() * 4
+    totals[1] += random.random() * 0.5
+    totals[2] += random.random() * 2
+    totals[3] += random.random()
+    all_totals = totals[0] + totals[1] + totals[2] + totals[3]
+    percentages = (totals[0] / all_totals * 100, totals[1] / all_totals * 100, totals[2] / all_totals * 100, totals[3] / all_totals * 100)
+    return totals, percentages
 
-n_groups = 4
+def animate(frameno):
+    global totals
+    global percentages
+    global ind
+    global width
+    totals, percentages = refresh_totals(totals)
+    for index, rect in enumerate(rects):
+        rect.set_height(percentages[index])
+    return plt.bar(ind, percentages, width, color='r',)
+
+N = 4
+ind = np.arange(N)
+width = 0.35
+
+totals, percentages = refresh_totals(totals)
 
 fig, ax = plt.subplots()
-index = np.arange(n_groups)
-bar_width = 0.35
-opacity = 0.4
-error_config = {'ecolor': '0.3'}
 
-try:
-    while True:
-        try:
-            response1 = float(ser1.readline())
-            response2 = float(ser2.readline())
-            response3 = float(ser3.readline())
-            response4 = float(ser4.readline())
+ax.set_ylabel('Percentage of time talking')
+ax.set_ylim(0, 100)
+ax.set_xticks(ind)
+ax.set_xticklabels(('A', 'B', 'C', 'D',))
 
-        total1 += response1
-        total2 += response2
-        total3 += response3
-        total4 += response4
-        # add the double value to each total
+rects = plt.bar(ind, percentages, width, color='r',)
 
-        totals = (total1, total2, total3, total4)
+ani = animation.FuncAnimation(fig, animate, blit = True, repeat = True)
 
-        # display the contributions with an updating bar graph
-
-        # rects1 = ax.bar(index, totals, bar_width, alpha = opacity, color = 'b', error_kw = error_config)
-
-        # ax.set_xlabel('Group member')
-        # ax.set_ylabel('Total time talking')
-        # ax.set_title('Scores by group and gender')
-        # ax.set_xticks(index + bar_width / 2)
-        # ax.set_xticklabels(('A', 'B', 'C', 'D'))
-        # ax.legend()
-
-        # fig.tight_layout()
-        # plt.show()
-
-        # ser1.close()
-        # ser2.close()
-        # ser3.close()
-        # ser4.close()
-except:
-    sys.exit(0)
+plt.show()
