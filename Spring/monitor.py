05d7b54966ebb764d834c74c22023a3b546feef9
Glenn M. Davis <gmdavis1@gmail.com>
2018-04-25 18:07:03 -0700

    working monitor with 2 arduino inputs through usb

--- Spring/monitor.py
+++ Spring/monitor.py
@@ -4,8 +4,8 @@ import matplotlib.animation as animation
 import numpy as np
 import random
 
-# ser1 = serial.Serial("/dev/ttyACM0")
-# ser2 = serial.Serial("/dev/ttyACM1")
+ser1 = serial.Serial("/dev/ttyUSB0")
+ser2 = serial.Serial("/dev/ttyUSB1")
 # ser3 = serial.Serial("/dev/ttyACM2")
 # ser4 = serial.Serial("/dev/ttyACM3")
 
@@ -17,12 +17,25 @@ totals = [total1, total2, total3, total4]
 
 def refresh_totals(totals):
     # add the float from Arduino for each totals[], now just simulated data
-    totals[0] += random.random() * 4
-    totals[1] += random.random() * 0.5
-    totals[2] += random.random() * 2
-    totals[3] += random.random()
+    add1 = ser1.readline()
+    try:
+        totals[0] += int(add1)
+    except:
+        pass
+    print("A adding ")
+    print(add1)
+    add2 = ser2.readline()
+    try:
+        totals[1] += int(add2)
+    except:
+        pass
+    print("B adding ")
+    print(add2)
+    totals[2] += 0
+    totals[3] += 0
     all_totals = totals[0] + totals[1] + totals[2] + totals[3]
     percentages = (totals[0] / all_totals * 100, totals[1] / all_totals * 100, totals[2] / all_totals * 100, totals[3] / all_totals * 100)
+    print('-' * 20)
     return totals, percentages
 
 def animate(frameno):
