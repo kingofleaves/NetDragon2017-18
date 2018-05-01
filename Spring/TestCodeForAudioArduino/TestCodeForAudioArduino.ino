1b748462ffdb2d0164cd2a2834d6805604d815d6
kingofleaves <wangye@stanford.edu>
2018-04-30 17:31:08 -0700

    Now samples in 0.1sec chunks and outputs percentage speaking time over the past 10 seconds

--- Spring/TestCodeForAudioArduino/TestCodeForAudioArduino.ino
+++ Spring/TestCodeForAudioArduino/TestCodeForAudioArduino.ino
@@ -1,8 +1,10 @@
 #define BUFFER_SIZE 100
-#define MIC_THRESHOLD 100
+#define MIC_BIAS 0
+#define MIC_THRESHOLD 10
 
 int memAudio[BUFFER_SIZE];
-
+long lastPoll = 0;
+long lastMsg = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,25 +13,41 @@ void setup() {
 }
 
 void loop() {
-  // Read raw value from Analog Pin to Mic
-  int rawVal = analogRead(0);
-  // Normalize DC Bias
-  int normVal = rawVal - 526;
-  // Print for Debugging
-  // Serial.print("Normalized Mic Value: ");
-  // Serial.println(normVal);
-  // Move all data in buffer back by one unit 
-  memmove(memAudio, memAudio + 1, sizeof(int) * (BUFFER_SIZE - 1));
-  // Store new value in buffer
-  memAudio[BUFFER_SIZE - 1] = (abs(normVal) > MIC_THRESHOLD ? 1 : 0);
 
-  // Send average speaking time to RPi
-  int speakTime = 0;
-  for (int i = 0; i < BUFFER_SIZE; i++) {
-    speakTime += memAudio[i];
-  }
-  Serial.print("speaking time; ");
-  Serial.println(speakTime);
+  // if(millis() - lastPoll > 100) {
+
+    int magVal = 0; // Stores magnitude of the next normalized reading
+
+    while(millis() - lastPoll < 100) { // detects speech for 1s. 
+      // Read raw value from Analog Pin to Mic
+      int rawVal = analogRead(0);
+      
+      // Serial.println(rawVal);
+      // Normalize DC Bias
+      int normVal = rawVal - MIC_BIAS;
+      // get Magnitude and store it if it's bigger than previous iteration.
+      magVal = max(magVal, abs(normVal));
+      // Print for Debugging
+      // Serial.print("Normalized Mic Value: ");
+      // Serial.println(normVal);
+    }
+    // Move all data in buffer back by one unit 
+    memmove(memAudio, memAudio + 1, sizeof(int) * (BUFFER_SIZE - 1));
+    // Store new value in buffer
+    memAudio[BUFFER_SIZE - 1] = (abs(magVal) > MIC_THRESHOLD ? 1 : 0);
+  
+    // Send average speaking time to RPi
+    int speakTime = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      speakTime += memAudio[i];
+    }
+    
+    //Serial.print("speaking time; ");
+    // TRY PRINTING THIS LESS?
+    Serial.println(speakTime); 
+
+    lastPoll = millis();
+  //}
   
 }
 
