7704c3757326d74e0bbffd00d0f22414efbdeb00
Glenn M. Davis <gmdavis1@gmail.com>
2018-05-23 21:37:49 -0700

    what

--- Spring/hw_bazaar/hw_bazaar.py
+++ Spring/hw_bazaar/hw_bazaar.py
@@ -1,8 +1,9 @@
 import os
 from guizero import App, Text, PushButton
 
-def ask(person):
-    message.value = "Asking " + person + " for help"
+def ask():
+    message.value = "Asking for help"
+    '''
     if person == 'A':
         os.system('mpg123 -q A.mp3 &')
     elif person == 'B':
@@ -11,14 +12,17 @@ def ask(person):
         os.system('mpg123 -q C.mp3 &')
     elif person == 'D':
         os.system('mpg123 -q D.mp3 &')
-    os.system('mpg123 -q can_you_answer.mp3 &')
+    '''
+    os.system('mpg123 can_you_answer.mp3 &')
 
 
 app = App(title = "GROUPR: Hardware Bazaar")
 message = Text(app, text = "Welcome to ME310 Hardware Bazaar 2018")
+'''
 ask_a = PushButton(app, command = ask("A"), text = "Ask A")
 ask_b = PushButton(app, command = ask("B"), text = "Ask B")
 ask_c = PushButton(app, command = ask("C"), text = "Ask C")
 ask_d = PushButton(app, command = ask("D"), text = "Ask D")
-os.system('mpg123 -q hello.mp3 &')
+'''
+ask = PushButton(app, command = ask(), text = "ask")
 app.display()
