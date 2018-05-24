d430300c54f63c02ff7dfcfe0b7fcb032f345cbf
Glenn M. Davis <gmdavis1@gmail.com>
2018-05-23 18:50:13 -0700

    testing hw_bazaar

--- Spring/hw_bazaar.py
+++ Spring/hw_bazaar.py
@@ -1,4 +1,9 @@
-from guizero import App
+from guizero import App, Text, PushButton
+
+def f1():
+    message.value = "Function 1"
 
 app = App(title = "GROUPR: Hardware Bazaar")
+message = Text(app, text = "Welcome to ME310 Hardware Bazaar 2018")
+function_1 = PushButton(app, command = f1, text = "Function 1")
 app.display()
