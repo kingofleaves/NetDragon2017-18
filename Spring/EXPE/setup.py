305fb8135568a8178567f9da31023db19b6b337c
Glenn M. Davis <gmdavis1@gmail.com>
2018-06-03 17:23:04 -0700

    setup.py works fine for capturing names

--- Spring/EXPE/setup.py
+++ Spring/EXPE/setup.py
@@ -3,8 +3,6 @@
 import speech_recognition as sr
 from gtts import gTTS
 import os
-import tkinter
-import time
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
@@ -14,25 +12,23 @@ BING_KEY = "94a54eca13074b93958fd66fd9c968bf"  # Microsoft Bing Voice Recognitio
 # Free: https://www.microsoft.com/cognitive-services/en-us/subscriptions?productId=/products/Bing.Speech.Preview
 # Paid: https://portal.azure.com/#create/Microsoft.CognitiveServices/apitype/Bing.Speech/pricingtier/S0
 
-root = tkinter.Tk()
 language = 'en'
 
 def speak(text):
-  print(text + '\n')
+  print(text)
   tts = gTTS(text, language)
   filename = 'temp.mp3'
   tts.save(filename)
   os.system('mpg123 -q ' + filename)
   os.remove(filename)
 
-def record(filename):
+def listen_record(filename):
   audio = r.listen(source)
   try:
     name = r.recognize_bing(audio, key=BING_KEY)
     tts = gTTS(name, language)
     tts.save(filename)
-    response = 'Your name is ' + name
-    speak(response)
+    os.system('mpg123 -q ' + filename)
     return name
   except sr.UnknownValueError:
     speak("Sorry, I didn't understand that.")
@@ -40,6 +36,31 @@ def record(filename):
   except sr.RequestError as e:
     speak("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
     return None
+
+def type_record(filename):
+  name = raw_input('Enter the input for ' + filename + ': ')
+  try:
+    tts = gTTS(name, language)
+    tts.save(filename)
+    if confirm(filename):
+      return name
+    else:
+      return None
+  except:
+    return None
+
+def confirm(filename):
+  os.system('mpg123 -q ' + filename)
+  choice = raw_input('Is this correct? [Y/n]: ').lower()
+  while True:
+    if choice == 'y' or choice == 'yes' or choice == '':
+      return True
+    elif choice == 'n' or choice == 'no':
+      return False
+    else:
+      print("Please choose 'y' or 'n'.")
+      choice = raw_input('Is this correct? [Y/n]: ').lower()
+
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -49,27 +70,27 @@ with sr.Microphone() as source:
   C_NAME = None
   D_NAME = None
   while not (A_NAME and B_NAME and C_NAME and D_NAME):
-    if not A_NAME:
+    while not A_NAME:
       speak("A, what is your name?")
       try:
-        A_NAME = record('A.mp3')
+        A_NAME = type_record('A.mp3')
       except:
         pass
-    if not B_NAME:
+    while not B_NAME:
       speak("B, what is your name?")
       try:
-        B_NAME = record('B.mp3')
+        B_NAME = type_record('B.mp3')
       except:
         pass
-    if not C_NAME:
+    while not C_NAME:
       speak("C, what is your name?")
       try:
-        C_NAME = record('C.mp3')
+        C_NAME = type_record('C.mp3')
       except:
         pass
-    if not D_NAME:
+    while not D_NAME:
       speak("D, what is your name?")
       try:
-        D_NAME = record('D.mp3')
+        D_NAME = type_record('D.mp3')
       except:
         pass
